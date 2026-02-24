3e3fa0a4c17e260961257e1b7c809a677cb6e207
diegobianqui <diego.bianqui@gmail.com>
2026-02-24 11:50:27 -0300

    Solution to challenge 02

--- test/functional/wallet_rpc_basics.py
+++ test/functional/wallet_rpc_basics.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# Copyright (c) 2015-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#!/usr/bin/env python3
 from decimal import Decimal
 from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal
